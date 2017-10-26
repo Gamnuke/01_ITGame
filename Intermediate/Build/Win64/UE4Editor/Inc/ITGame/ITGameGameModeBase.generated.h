9f4500617abf9e95fa275660a1b99569520327df
ShanukaDW720 <shanuka2003@gmail.com>
2017-10-26 21:21:46 +1100

    Transfering to pc

---
+++ Intermediate/Build/Win64/UE4Editor/Inc/ITGame/ITGameGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ITGAME_ITGameGameModeBase_generated_h
+#error "ITGameGameModeBase.generated.h already included, missing '#pragma once' in ITGameGameModeBase.h"
+#endif
+#define ITGAME_ITGameGameModeBase_generated_h
+
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_RPC_WRAPPERS
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAITGameGameModeBase(); \
+	friend ITGAME_API class UClass* Z_Construct_UClass_AITGameGameModeBase(); \
+public: \
+	DECLARE_CLASS(AITGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/ITGame"), NO_API) \
+	DECLARE_SERIALIZER(AITGameGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAITGameGameModeBase(); \
+	friend ITGAME_API class UClass* Z_Construct_UClass_AITGameGameModeBase(); \
+public: \
+	DECLARE_CLASS(AITGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), 0, TEXT("/Script/ITGame"), NO_API) \
+	DECLARE_SERIALIZER(AITGameGameModeBase) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AITGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AITGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AITGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AITGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AITGameGameModeBase(AITGameGameModeBase&&); \
+	NO_API AITGameGameModeBase(const AITGameGameModeBase&); \
+public:
+
+
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AITGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AITGameGameModeBase(AITGameGameModeBase&&); \
+	NO_API AITGameGameModeBase(const AITGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AITGameGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AITGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AITGameGameModeBase)
+
+
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_12_PROLOG
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ITGame_Source_ITGame_ITGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ITGame_Source_ITGame_ITGameGameModeBase_h_15_RPC_WRAPPERS \
+	ITGame_Source_ITGame_ITGameGameModeBase_h_15_INCLASS \
+	ITGame_Source_ITGame_ITGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ITGame_Source_ITGame_ITGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ITGame_Source_ITGame_ITGameGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	ITGame_Source_ITGame_ITGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	ITGame_Source_ITGame_ITGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	ITGame_Source_ITGame_ITGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ITGame_Source_ITGame_ITGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
